6b6d71f4a5adc1a21e59a6dc1daf6dfc6c5a20b5
Soulsaw <ssawadogo292@gmail.com>
2023-04-28 21:38:08 -0400

    fix correct output

--- main.h
+++ main.h
@@ -4,9 +4,12 @@
 #include <unistd.h>
 #include <stdarg.h>
 int _printf(const char *format, ...);
-void print_conversion_hexdecimal(char choice, int *a, va_list ap, int *b, int *len);
-void print_conversion_octal(char choice, int *a, va_list ap, int *b, int *len);
-void print_conversion_binary(char choice, int *a, va_list ap, int *b, int *len);
+void print_conversion_hexdecimal(char choice, int *a,
+		va_list ap, int *b, int *len);
+void print_conversion_octal(char choice, int *a,
+		va_list ap, int *b, int *len);
+void print_conversion_binary(char choice, int *a,
+		va_list ap, int *b, int *len);
 void print_conversion_int(char choice, int *a, va_list ap, int *b, int *len);
 void print_conversion(char choice, int *a, va_list ap, int *b, int *len);
 void print_binary_digit(long int n, int *len);
